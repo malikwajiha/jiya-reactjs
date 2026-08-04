3a8fa7d2d85378343ec93815b891eaaae90fc35e
wajiha malik <malikwajiha373@gmail.com>
2026-08-04 21:14:35 +0500

    08miniContext API

---
+++ 08miniContext/src/Components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useContext } from 'react'
+import userContext from '../context/userContext'
+
+function Profile() {
+    const {user} = useContext(userContext)
+    if (!user) {
+        return<div>Please Login</div>
+    }else {
+        return <div> Welcome {user.username}</div>
+    }
+    
+}
+
+export default Profile
---
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import userContext from "./userContext";
+import { useState } from "react";
+
+
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = useState([])
+    return(
+        <userContext.Provider value={{user, setUser}}>
+            {children}
+
+        </userContext.Provider>
+    )
+}
+
+
+export default UserContextProvider
