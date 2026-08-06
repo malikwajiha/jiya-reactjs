17851cdb519eacb991f8910a5804e2ee9c9a6491
wajiha malik <malikwajiha373@gmail.com>
2026-08-07 02:55:45 +0500

    fix todo add functionality

--- 10todocontextLocalStorage/src/App.jsx
+++ 10todocontextLocalStorage/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     const [ todos, setTodos] = useState([])
 
     const addTodo = (todo) => {
-      setTodos((prev) => [{id: Date.now(), ...todo,...prev}])
+      setTodos((prev) => [{id: Date.now(), ...todo},...prev])
     }
 
     const updateTodo = (todo, id ) => {
