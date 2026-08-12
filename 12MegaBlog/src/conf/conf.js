57684707847b1bd014e1e15049b76355a72efb40
wajiha malik <malikwajiha373@gmail.com>
2026-08-12 16:06:31 +0500

    Add MegaBlog authentication and project files

---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,12 @@
+const conf ={
+
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteTablesDbId: String(import.meta.env.VITE_APPWRITE_TABLESDB_ID),
+    appwriteTableId: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+
+}
+
+
+export default conf
